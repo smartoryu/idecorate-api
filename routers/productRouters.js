27061a537844e5179d743642e79dc88acb6d423f
smartoryu <prikenang@gmail.com>
2020-02-27 16:58:12 +0700

    add getProducts and getImages router

--- routers/productRouters.js
+++ routers/productRouters.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { productController } = require("../controllers");
 
+router.get("/get_products/:storeid", productController.getProducts);
+router.get("/get_images/:productid", productController.getImages);
 router.post("/add", productController.postProduct);
 
 module.exports = router;
