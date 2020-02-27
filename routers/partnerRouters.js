6cc906d0b30ba119be065f777af644797116e81d
smartoryu <prikenang@gmail.com>
2020-02-27 16:56:58 +0700

    add (put) edit/:storeid router

--- routers/partnerRouters.js
+++ routers/partnerRouters.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const { partnerController } = require("../controllers");
 
 router.get("/", partnerController.getStore);
+router.put("/edit/:storeid", partnerController.updateStore);
 
 module.exports = router;
