3a49501ea469ac63393f401fa97ca597f9f2ca67
smartoryu <prikenang@gmail.com>
2020-02-28 14:01:57 +0700

    adding new controller

--- routers/productRouters.js
+++ routers/productRouters.js
@@ -3,6 +3,9 @@ const { productController } = require("../controllers");
 
 router.get("/get_products/:storeid", productController.getProducts);
 router.get("/get_images/:productid", productController.getImages);
-router.post("/add", productController.postProduct);
+
+router.post("/add/:storeid", productController.postProduct);
+
+router.delete("/delete/:storeid/:productid", productController.deleteProduct);
 
 module.exports = router;
