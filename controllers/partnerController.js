1116d12838603615d0ca28d388f221668bff82fc
smartoryu <prikenang@gmail.com>
2020-02-28 14:02:16 +0700

    minor changes

--- controllers/partnerController.js
+++ controllers/partnerController.js
@@ -7,16 +7,18 @@ module.exports = {
   getStore: (req, res) => {
     const { userid } = req.query;
 
-    try {
-      let sql = `SELECT * FROM stores WHERE userid = ${userid}`;
-      mysqldb.query(sql, (err, resStore) => {
-        if (err) res.status(500).send(err);
+    if (userid > 0) {
+      try {
+        let sql = `SELECT * FROM stores WHERE userid = ${userid}`;
+        mysqldb.query(sql, (err, resStore) => {
+          if (err) res.status(500).send(err);
 
-        return res.status(200).send({ result: resStore[0] });
-      });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ message: "There's an error on the server. Please contact the administrator." });
+          return res.status(200).send({ result: resStore[0] });
+        });
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "There's an error on the server. Please contact the administrator." });
+      }
     }
   },
   updateStore: (req, res) => {
